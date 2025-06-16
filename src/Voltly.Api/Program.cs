b1b0ea82dd7c242100d23a767b0d881d74d36f25
esythir <dev.brunamassi@gmail.com>
2025-06-16 01:44:51 -0300

    feat(user): add controllers, dtos, validators, mappers, repositories

--- src/Voltly.Api/Program.cs
+++ src/Voltly.Api/Program.cs
@@ -1,10 +1,9 @@
 using Microsoft.EntityFrameworkCore;
-using Voltly.Api.Extensions;           // extensão que registra toda a infraestrutura
+using Voltly.Api.Extensions;
 using Voltly.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// registra DbContext, UoW, Mapster, MediatR, FluentValidation, repositórios, etc.
 builder.Services.AddVoltlyInfrastructure(builder.Configuration);
 
 // MVC + Swagger
@@ -14,7 +13,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// aplica migrações na primeira subida
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<VoltlyDbContext>();

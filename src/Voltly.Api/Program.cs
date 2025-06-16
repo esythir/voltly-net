8e3d68731d601aaeb86e4a1587dd448338630ade
esythir <dev.brunamassi@gmail.com>
2025-06-16 01:44:31 -0300

    feat(user): add controllers, dtos, validators, mappers, repositories

--- src/Voltly.Api/Program.cs
+++ src/Voltly.Api/Program.cs
@@ -1,55 +1,28 @@
-using Mapster;
-using MediatR;
-using FluentValidation;
-using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
-using Voltly.Application.Abstractions;
-using Voltly.Application.Mapping;
+using Voltly.Api.Extensions;           // extensão que registra toda a infraestrutura
 using Voltly.Infrastructure.Persistence;
-using Voltly.Infrastructure.Repositories;
-using Voltly.Application.Features.Users.Commands.CreateUser;
 
 var builder = WebApplication.CreateBuilder(args);
-var cfg     = builder.Configuration;
-var connStr = cfg.GetConnectionString("Oracle");
 
-// 1) DbContext com Lazy-Loading só em runtime
-builder.Services.AddDbContext<VoltlyDbContext>(opt =>
-    opt.UseOracle(connStr,
-            o => o.MigrationsAssembly(typeof(VoltlyDbContext).Assembly.FullName))
-        .UseLazyLoadingProxies());
+// registra DbContext, UoW, Mapster, MediatR, FluentValidation, repositórios, etc.
+builder.Services.AddVoltlyInfrastructure(builder.Configuration);
 
-// 2) UoW e Repositório genérico
-builder.Services.AddScoped<IUnitOfWork, VoltlyDbContext>();
-builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
-
-// 3) Mapster
-var mapsterCfg = TypeAdapterConfig.GlobalSettings;
-MapsterConfig.Configure(mapsterCfg);
-builder.Services.AddSingleton(mapsterCfg);
-builder.Services.AddScoped<IMapper, ServiceMapper>();
-
-// 4) MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateUserCommand>());
-
-// 5) FluentValidation
-builder.Services
-    .AddFluentValidationAutoValidation()
-    .AddValidatorsFromAssemblyContaining<CreateUserValidator>();
-
-// 6) Controllers + Swagger
+// MVC + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 7) Auto-migrations ao subir
-using var scope = app.Services.CreateScope();
-var db = scope.ServiceProvider.GetRequiredService<VoltlyDbContext>();
-db.Database.Migrate();
+// aplica migrações na primeira subida
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<VoltlyDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
 app.MapControllers();
+
 app.Run();
